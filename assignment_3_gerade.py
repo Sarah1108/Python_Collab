0134513cd5ad1b0fa2bf42b4fad122278e3a8d35
Sarah1108 <sarah.sulollari@gmail.com>
2021-03-18 16:36:20 +0100

    erster versuch

---
+++ assignment_3_gerade.py
@@ -0,0 +1,57 @@
+####################################################################################################
+#2
+#####################################################################################################
+import math
+
+def distance(ax,ay,bx,by):
+    step1= ((ax-bx)**2) + (ay-by)**2
+    dis = math.sqrt(step1)
+    print(dis)
+distance(1,3,4,2)
+#######################################################################################################
+#4
+########################################################################################################
+
+roman_numerals = {"M":1000,"CM":900,"D":500,"CD":400,"C":100,"XC":90,"L":50,"XL":40,"X":10,"V":5,"IV":4,"I":1}
+
+def Roman_to_int(t):
+    
+    resultI = 0
+    for k,v in roman_numerals.items():          
+        if t == k:
+            resultI += roman_numerals.get(t)
+        else:
+            for i in t:
+                if i in roman_numerals.keys():
+                    if i == k:
+                        resultI += v
+    print(resultI)
+Roman_to_int("M")
+
+roman_numerals = {"M":1000,"CM":900,"D":500,"CD":400,"C":100,"XC":90,"L":50,"XL":40,"X":10,"V":5,"IV":4,"I":1}
+
+def Roman_to_int(t):
+    
+    resultI = 0
+    for k,v in roman_numerals.items():          
+        if t == k:
+            resultI += roman_numerals.get(t)
+        else:
+            for i in t:
+                if i in roman_numerals.keys():
+                    if i == k:
+                        resultI += v
+    print(resultI)
+Roman_to_int("M")
+#####not finished yet
+def int_to_Roman(t):
+    result2 = 0 
+    for k,v in roman_numerals.items():
+        if t == v:
+            result2 = roman_numerals.get(t)
+    print(result2)
+int_to_Roman(50)
+
+##################################################################################################
+#6
+##################################################################################################
---
+++ assignment_4_gerade.py
@@ -0,0 +1,78 @@
+
+##########################################################################################
+#2
+##########################################################################################
+
+def sort(d):
+    d_sorted = {k: v for k, v in sorted(d.items(), key=lambda x: x[1])}
+    print(d_sorted)
+sort(d)
+
+###########################################################################################
+# 4
+###########################################################################################
+
+class Triangle:
+    def __init__(self, side1, side2, side3):
+        self.side1 = side1
+        self.side2 = side2
+        self.side3 = side3
+        print('running __init__\n')
+        self.type_of_triangle()
+        
+    def __repr__(self):
+        return f"I am a Triangle with sides {self.side1}, {self.side2} and {self.side3}"
+        
+    #def __repr__(self):
+    #    return "42"
+        
+    def type_of_triangle(self):
+        
+        if self.side1 == self.side2 and self.side1 == self.side3:
+            print('1')## equilaterian
+            self.mytype = 'equilateral'
+        elif self.side1 == self.side2 or \
+             self.side1 == self.side3 or \
+             self.side2 == self.side3:
+            print('2') #isosceles
+            self.mytype = 'isosceles'
+        elif self.side1**2 + self.side2**2 != self.side3**2:
+            print('0')
+            self.mytype = 'no triangle'
+        else:
+            print('3')
+            self.mytype = 'scalene'
+tri= Triangle(5,5,5)
+
+###############################################################################################
+#6
+###############################################################################################
+class employee:
+    def __init__(self, name, ssid, salary, department):
+        self.name= name
+        self.ssid= ssid
+        self.salary = salary
+        self.department = department
+        self.bonus()
+    def bonus(self):
+        self.newsalary = salary*1,1
+
+class manager(employee):
+    def __init__(self, name, ssid, salary, department, password, numberOfEmployees):
+        self.name= name
+        self.ssid= ssid
+        self.salary = salary
+        self.department = department
+        self.bonus()
+        self.password = password
+        self.numberOfEmployees = numberOfEmployees
+        self.authenticate_password = 
+    def bonus():
+        self.newsalary = salary*1,15
+    def authenticate_password(password):
+        x = input("Enter your password: ")
+        if x == password:
+            True
+        else:
+            False
+    
