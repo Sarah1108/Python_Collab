9c8e5f8c3dd5174caf9e6310f1c26eb6548c69a4
Sarah1108 <sarah.sulollari@gmail.com>
2021-03-18 21:25:11 +0100

    last try

--- assignment_3_gerade.py
+++ assignment_3_gerade.py
@@ -1,43 +1,67 @@
 ####################################################################################################
 #2
 #####################################################################################################
-#import math
+import math
 
-#def distance(ax,ay,bx,by):
-#    step1= ((ax-bx)**2) + (ay-by)**2
-#    dis = math.sqrt(step1)
-#    print(dis)
-#distance(1,3,4,2)
+def distance(ax,ay,bx,by):
+    step1= ((ax-bx)**2) + (ay-by)**2
+    dis = math.sqrt(step1)
+    print(dis)
+distance(1,3,4,2)
 #######################################################################################################
 #4
 ########################################################################################################
 
-#roman_numerals = {"M":1000,"CM":900,"D":500,"CD":400,"C":100,"XC":90,"L":50,"XL":40,"X":10,"V":5,"IV":4,"I":1}
+roman_numerals = {"M":1000,"CM":900,"D":500,"CD":400,"C":100,"XC":90,"L":50,"XL":40,"X":10,"V":5,"IV":4,"I":1}
 
-#def Roman_to_int(t):
+def Roman_to_int(t):
     
-#    resultI = 0
-#    for k,v in roman_numerals.items():          
-#        if t == k:
-#            resultI += roman_numerals.get(t)
-#        else:
-#            for i in t:
-#                if i in roman_numerals.keys():
-#                    if i == k:
- ##                       resultI += v
-#    print(resultI)
-#Roman_to_int("M")
+    resultI = 0
+    for k,v in roman_numerals.items():          
+        if t == k:
+            resultI += roman_numerals.get(t)
+        else:
+            for i in t:
+                if i in roman_numerals.keys():
+                    if i == k:
+                       resultI += v
+    print(resultI)
+Roman_to_int("M")
 
-#roman_numerals = {"M":1000,"CM":900,"D":500,"CD":400,"C":100,"XC":90,"L":50,"XL":40,"X":10,"V":5,"IV":4,"I":1}
+int_roman = {1000:"M",900:"CM",500:"D",400:"CD",100:"C",90:"XC",50:"L",40:"XL",10:"X",5:"V",4:"IV",1:"I"}
 
 #####not finished yet
-#def int_to_Roman(t):
-#    result2 = 0 
-#    for k,v in roman_numerals.items():
-#        if t == v:
-#            result2 = roman_numerals.get(t)
-#    print(result2)
-#int_to_Roman(50)
+def int_to_Roman(t):
+    result2 = "" 
+    for k,v in int_roman.items():
+        if t == v:
+            result2 += int_roman.get(v)
+        elif result2 == "":
+            for i in int_roman.keys():
+                if i == k:
+                    result2 += v
+    print(result2)
+int_to_Roman(1500)
+
+#########################tried this from : https://www.oreilly.com/library/view/python-cookbook/0596001673/ch03s24.html
+#########################didn't work either
+#def int_to_roman(input):
+    """ Convert an integer to a Roman numeral. """
+
+#    if not isinstance(input, type(1)):
+#        raise TypeError ("expected integer, got %s" % type(input))
+#    if not 0 < input < 4000:
+#        raise ValueError ("Argument must be between 1 and 3999")
+#    ints = (1000, 900,  500, 400, 100,  90, 50,  40, 10,  9,   5,  4,   1)
+#    nums = ('M',  'CM', 'D', 'CD','C', 'XC','L','XL','X','IX','V','IV','I')
+#    result = []
+#    for i in range(len(ints)):
+#        count = int(input / ints[i])
+#        result.append(nums[i] * count)
+#        input -= ints[i] * count
+#    return ''.join(result) 
+#    print(result)
+#int_to_roman(50)
 
 ##################################################################################################
 #6
--- assignment_4_gerade.py
+++ assignment_4_gerade.py
@@ -3,62 +3,67 @@
 #2
 ##########################################################################################
 
-def sort(d):
-    d_sorted = {k: v for k, v in sorted(d.items(), key=lambda x: x[1])}
-    print(d_sorted)
-sort(d)
+#def sort(d):
+#    d_sorted = {k: v for k, v in sorted(d.items(), key=lambda x: x[1])}
+#    print(d_sorted)
+#sort(d)
 
 ###########################################################################################
 # 4
 ###########################################################################################
 
-class Triangle:
-    def __init__(self, side1, side2, side3):
-        self.side1 = side1
-        self.side2 = side2
-        self.side3 = side3
-        print('running __init__\n')
-        self.type_of_triangle()
+#class Triangle:
+#    def __init__(self, side1, side2, side3):
+#        self.side1 = side1
+#        self.side2 = side2
+#        self.side3 = side3
+#        print('running __init__\n')
+#        self.type_of_triangle()
         
-    def __repr__(self):
-        return f"I am a Triangle with sides {self.side1}, {self.side2} and {self.side3}"
+#    def __repr__(self):
+#        return f"I am a Triangle with sides {self.side1}, {self.side2} and {self.side3}"
         
     #def __repr__(self):
     #    return "42"
         
-    def type_of_triangle(self):
-        
-        if self.side1 == self.side2 and self.side1 == self.side3:
-            print('1')## equilaterian
-            self.mytype = 'equilateral'
-        elif self.side1 == self.side2 or \
-             self.side1 == self.side3 or \
-             self.side2 == self.side3:
-            print('2') #isosceles
-            self.mytype = 'isosceles'
-        elif self.side1**2 + self.side2**2 != self.side3**2:
-            print('0')
-            self.mytype = 'no triangle'
-        else:
-            print('3')
-            self.mytype = 'scalene'
-tri= Triangle(5,5,5)
+#    def type_of_triangle(self):
+#        
+#        if self.side1 == self.side2 and self.side1 == self.side3:
+#            print('1')## equilaterian
+#            self.mytype = 'equilateral'
+#        elif self.side1 == self.side2 or \
+#             self.side1 == self.side3 or \
+#             self.side2 == self.side3:
+#            print('2') #isosceles
+##            self.mytype = 'isosceles'
+#        elif self.side1**2 + self.side2**2 != self.side3**2:
+#            print('0')
+#            self.mytype = 'no triangle'
+#        else:
+#            print('3')
+#            self.mytype = 'scalene'
+#tri= Triangle(5,5,5)
 
 ###############################################################################################
 #6
 ###############################################################################################
+##AKTUEL:
 class employee:
-    def __init__(self, name, ssid, salary, department):
-        self.name= name
-        self.ssid= ssid
+    def __init__(self, name, ssid, salary, department): #password= None, numberOfEmployees= 0):
+        self.name = name
+        self.ssid = ssid
         self.salary = salary
         self.department = department
         self.bonus()
     def bonus(self):
-        self.newsalary = salary*1,1
+        self.newsalary = self.salary*1,1
+        return self.newsalary
+    #def name(self):
+        #return self.name
 
 class manager(employee):
     def __init__(self, name, ssid, salary, department, password, numberOfEmployees):
+        employee.__init__(self, name, ssid, salary, department)#, password, numberOfEmployees)
         self.name= name
         self.ssid= ssid
         self.salary = salary
@@ -67,8 +72,57 @@ class manager(employee):
         self.password = password
         self.numberOfEmployees = numberOfEmployees
         self.authenticate_password
-    def bonus():
-        self.newsalary = salary*1,15
-    def authenticate_password(x):
-        valid = x == password
-        print(valid)
+    def bonus(self):
+        self.newsalary = self.salary*1,15
+        return self.newsalary
+    def authenticate_password(password1, self):
+        if password1 == self.password is False:
+            password_authentification = False
+        else: 
+            passworkd_authentificatio = True 
+
+f1 = employee("John",12345678900,2500,"TI")
+f2 = employee("Paul",12345678901,1800,"TI")
+f3 = manager("Marta",23456789012,6000,"TI",101101,2)##instead of employee
+# It didn't work with f1.name(), or any argument(), so we removed them
+#f1.name
+#f2.ssid
+#f3.department
+############## only worked when class f3 changed to manger instead of employee as that class does not have authenticate_password
+#f3.bonus()
+f3.authenticate_password(101101)
+
+####ALTE VERSION#################################################################
+#################################################################################################################
+#class employee:
+ #   def __init__(self, name, ssid, salary, department, password= None, numberOfEmployees= 0):
+  #      self.name = name
+   #     self.ssid = ssid
+    #    self.salary = salary
+     #   self.department = department
+       # self.bonus()
+    #d#ef bonus(self):
+    #    self.newsalary = self.salary*1,1
+    #def name(self):
+    #    #return self.name
+
+#class manager(employee):
+#    def __init__(self, name, ssid, salary, department, password, numberOfEmployees):
+#        self.name= name
+#        self.ssid= ssid
+#        self.salary = salary
+#        self.department = department
+#        self.bonus()
+#        self.password = password
+#        self.numberOfEmployees = numberOfEmployees
+#        self.authenticate_password
+#    def bonus(self):
+#        self.newsalary = self.salary*1,15
+#    def authenticate_password(password1, self):
+#       password1 == self.password
+
+#f1 = employee("John",12345678900,2500,"TI")
+#f2 = employee("Paul",12345678901,1800,"TI")
+#f3 = employee("Marta",23456789012,6000,"TI",101101,2)
+
+#f2.bonus() 
